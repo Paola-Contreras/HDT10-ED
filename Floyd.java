40387ecc0c33670d1157052d9c58e9c8dcedb64b
Gabriela Paola Contreras Guerra <paolacontreras231@gmail.com>
2021-05-29 15:28:59 -0600

    Avances

--- Floyd.java
+++ Floyd.java
@@ -3,25 +3,30 @@
     Gabriela Paola Contreras Guerra
     Carné: 20213*/
 
-    import java.util.*;
-    import java.util.Vector;
+// Imports 
+import java.util.*;
+import java.util.Vector;
 
 public class Floyd {
     
+    // Atributs 
     private Vector<String> Ubicacion;
     private int[][] distancia;
     private int[][] ruta;
-
+    
+    /**
+     * Constructor method that sets the quantity of the matrix
+     */
     public Floyd(){
+        // Instances 
         Ubicacion= new  Vector<String>();
         distancia= new int [35][35];
         ruta= new int [35][35];
-
+        
+        //Loop that fills the matrix with infinites
         for (int i =0; i<35; i++){
             for (int j =0; j<35; j++){
-               
                 ruta [i][j]=100;
-               
                 if (i==j){
                     distancia[i][j]=0;
                 }else{
@@ -29,19 +34,28 @@ public class Floyd {
                 }
             }
         }
-
     }
+    
+    /**
+     * Method that fills the data into the matrix
+     * @param s First city
+     * @param a Second city
+     * @param d Distance 
+     */
     public void agregar(String s, String a, String d){
         int kms;
         int kma;
+        // if that checks if the vector does not contain the city entered by the user and then save it
         if (!Ubicacion.contains(s) || !Ubicacion.contains(a)){
-            Ubicacion.add(s);
-            Ubicacion.add(a);
+            Ubicacion.add(s); // add the first city into the vector 
+            Ubicacion.add(a); // add the second city into the vector 
         }
-
+        // get the index of the cities entered by the user 
         kms=Ubicacion.indexOf(s);
         kma=Ubicacion.indexOf(a);
+        // Converts the string of distance into a int 
         distancia[kms][kma]= Integer.parseInt(d);
+        // conditional to save the distance into the matrix 
         if (d=="100"){
             ruta[kms][kma]=100;
         }else{
@@ -49,21 +63,34 @@ public class Floyd {
         }
     }
 
+    /**
+     * Method to corroborate if a city exist 
+     * @param s city number one 
+     * @param a city number two 
+     * @return
+     */
     public boolean existe(String s,String a){
         boolean esta= false; 
         if(Ubicacion.contains(s) && Ubicacion.contains(a)){
             esta=true;
-        }
-        return esta;
+        }return esta;
     }
 
+    /**
+     * Method that execute the floyd method 
+     */
     public void Shorter_rute(){
+        /*First for = repets the proces for each element in the vector
+          Second for = go through the rows
+          Third for =  go through the colums*/
         for (int i=0; i<Ubicacion.size();i++){
                for (int j = 0; j<Ubicacion.size(); j++){
                    for (int k = 0; k<Ubicacion.size(); k++){
+                       // contional that evaluates if the distance continues to be a infinite 
                     if (distancia[j][i]==100 || distancia[i][k]==100)
                     continue; // label that skips the remaining statements in the loop and proceeds to the next
                     if (distancia[j][i] + distancia[i][k]<distancia[j][k]){
+                        // changes the distance and rute  
                         distancia[j][k]=distancia[j][i] + distancia[i][k];
                         ruta[j][k]= ruta [j][i];
                     }
@@ -73,54 +100,77 @@ public class Floyd {
 
     }
 
-
+    /**
+     * Method to find the center of the graph 
+     */
     public void center(){
         int max=0;
-        Vector<Integer> centro = new  Vector<Integer>();
+        Vector<Integer> centro = new  Vector<Integer>(); // vector to save the data of the center 
         
         for (int i =0; i<Ubicacion.size();i++){
             for (int j =0; j<Ubicacion.size();j++){
+                // verify if the matrix is greater than 0 
                 if( distancia[i][j]>0){
-                    max = distancia[i][j];
+                    max = distancia[i][j]; // assing the greater number in the variable 
                 }
             }
-        centro.add(max);
+        centro.add(max); // add to the matrix the greater number 
         }
-        
-        int o = Collections.min(centro);
-        int ub= centro.indexOf(o);
-        String ub2= Ubicacion.get(ub);
+        int o = Collections.min(centro); // gets the minimum number in the vector 
+        int ub= centro.indexOf(o); // gets the index of the minimum number
+        String ub2= Ubicacion.get(ub); // gets the name of the city 
+        //Pints to show the data 
         System.out.println("\n-La excentridad minima es de: "+ o );
         System.out.println("El centro del grafo es: " + ub2);
     }
 
-
+    /**
+     * Metod to check what cities were used 
+     * @param s city number 1
+     * @param a city nimber 2 
+     */
     public void cerca(String s, String a){
-        int u1= Ubicacion.indexOf(s);
-        int u2= Ubicacion.indexOf(a);
+        int u1= Ubicacion.indexOf(s); // gets the index of the first city entered by the user
+        int u2= Ubicacion.indexOf(a); // gets the index of the second city entered by the user
+        //Shows the cities 
         print(u1, u2);
     }
-    
+
+    /**
+     *  Method to get the distance traveled
+     * @param scity number 1
+     * @param a city nimber 2 
+     * @return the km of the distance traveled 
+     */
     public int recorrido(String s, String a){
+        // int to saved the distance traveld acording to the cities entered by the user
         int recorrio= distancia[Ubicacion.indexOf(s)][Ubicacion.indexOf(a)];
         return recorrio;
     }
 
-    private void print (int k, int m){
-        Vector<String> camino = new Vector<String>();
-        camino.add(" -"+Ubicacion.get(k));
-
+    /**
+     * Method to print certain data of  matrix 
+     * @param k
+     * @param m
+     */
+    public void print (int k, int m){
+        Vector<String> camino = new Vector<String>();// vector to save the path used 
+        camino.add("\n--"+Ubicacion.get(k)); // add to the vector the first city 
+        //conditional that checks if it is possible to travel from one city to anotherone 
         if (distancia[k][m] == 100 || distancia[k][m] == 0){
-            System.out.println("No existe una conección entre ciudades\n"); 
+            // checks if the distance eaquals a infinite and shows a message 
+            System.out.println("\n****No existe una conección entre ciudades****\n"); 
         }else{
+            // if the distance is not infinite saves the data 
             while (k!= m){
                 k = ruta[k][m];
                 camino.add(Ubicacion.get(k));
             }
         }
-
-        int tamano = camino.size();
+        int tamano = camino.size(); // int that saves the size of the vector 
+        // cycle that prints the vector information as long as it is less than the amount of data that is stored
         for(int i = 0; i < tamano - 1; i++)
+        // prints the data of the vector 
         System.out.print(camino.get(i) + " \n - ");
         System.out.print(camino.get(tamano - 1) + "\n");
     }
--- Main.java
+++ Main.java
@@ -3,125 +3,130 @@
     Gabriela Paola Contreras Guerra
     Carné: 20213*/
 
-    //Imports
+//Imports
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-    public class Main{
-        public static void main(String[] args) throws FileNotFoundException{ 
-            
-            //Attributes 
-            Vista v = new Vista();
-            Floyd grafo = new Floyd();
-            Scanner scan = new Scanner(System.in);
-            int menusito  = 0;
-            int menusito2 = 0;
-            int menusito3 = 0;
-            boolean salir  = false;
-            boolean salir1 = false;
-            boolean salir2 = false;
-            String Csalida  = "";
-            String Cdestino = "";
-            String caminokm = "";
+public class Main{
+    public static void main(String[] args) throws FileNotFoundException{ 
+        
+        //Attributes 
+        Vista v = new Vista();
+        Floyd grafo = new Floyd();
+        Scanner scan = new Scanner(System.in);
+        int menusito  = 0;
+        int menusito2 = 0;
+        int menusito3 = 0;
+        boolean salir  = false;
+        boolean salir1 = false;
+        boolean salir2 = false;
+        String Csalida  = "";
+        String Cdestino = "";
+        String caminokm = "";
 
-        // Try & Catch that reads the txt file  and save it on an matrix separate by a space 
-            try{
-                File Info= new File("guategrafo.txt"); // file that want to be open 
-                Scanner readFile= new Scanner(Info);
+    // Try & Catch that reads the txt file  and save it on an matrix separate by a space 
+        try{
+            File Info= new File("guategrafo.txt"); // file that want to be open 
+            Scanner readFile= new Scanner(Info);
 
-                // Loop that reads if the document still have lines 
-                while(readFile.hasNextLine()){
-                    String Lineas = readFile.nextLine(); 
-                    String[] Separa = Lineas.split(" "); // Splits the document by space    
-                    grafo.agregar(Separa[0], Separa[1], Separa[2]); // Save the data into the matrix
-                }  
-            }catch(Exception e){
-               v.ErrorF();
-            }
+            // Loop that reads if the document still have lines 
+            while(readFile.hasNextLine()){
+                String Lineas = readFile.nextLine(); 
+                String[] Separa = Lineas.split(" "); // Splits the document by space    
+                grafo.agregar(Separa[0], Separa[1], Separa[2]); // Save the data into the matrix
+            }  
+        }catch(Exception e){
+            v.ErrorF();
+        }
 
-            // The program starts 
-            v.Welcome();
-            grafo.Shorter_rute(); // organize the matrix using the Floyd method
-           // loop that allows the program to always be running 
-            while (salir==false){
-                menusito= v.menu1(); // Main menu is shown 
-                if(menusito==1){
-                    // Distancia entre ciudades 
-                    while(salir2==false){
-                        menusito3=v.menu3(); // Sub menu to search again another rute 
-                        if(menusito3==1){
-                            // option that shows the shorter rute 
-                            v.Departure(); // Ask for the first city 
-                            Csalida=scan.nextLine();
+        // The program starts 
+        v.Welcome();
+        grafo.Shorter_rute(); // organize the matrix using the Floyd method
+        // loop that allows the program to always be running 
+        while (salir==false){
+            menusito= v.menu1(); // Main menu is shown 
+            if(menusito==1){
+                // Distancia entre ciudades 
+                while(salir2==false){
+                    menusito3=v.menu3(); // Sub menu to search again another rute 
+                    if(menusito3==1){
+                        // option that shows the shorter rute 
+                        v.Departure(); // Ask for the first city 
+                        Csalida=scan.nextLine();
 
-                            v.Arrive(); // Ask for the second city 
-                            Cdestino=scan.nextLine();
-                            // If that checks if both cities exist 
-                            if (grafo.existe(Csalida,Cdestino)){
-                                v.rute();
-                                grafo.cerca(Csalida, Cdestino); // Check the cities that arr coloser 
-                                int gg = grafo.recorrido(Csalida, Cdestino); // shows the amount of km that were used 
-                                System.out.println( "La distancia recorrida fue de: " + gg);
-                            }else{
-                                v.Error();
-                            }
-                        }if (menusito3==2){
-                            // Return main menu  
-                            v.back();
-                            salir2 =true;
+                        v.Arrive(); // Ask for the second city 
+                        Cdestino=scan.nextLine();
+                        // If that checks if both cities exist 
+                        if (grafo.existe(Csalida,Cdestino)){
+                            v.rute();
+                            grafo.cerca(Csalida, Cdestino); // Check the cities that are coloser 
+                            int gg = grafo.recorrido(Csalida, Cdestino); // shows the amount of km that were used 
+                            System.out.println( "La distancia recorrida fue de: " + gg);
+                        }else{
+                            v.Error();
                         }
+                    }if (menusito3==2){
+                        // Return main menu  
+                        v.back();
+                        salir2 =true;
                     }
-
-                }if (menusito==2){
-                    // Mostrar el centro del grafo 
-                    v.Cg();
-                    grafo.center();
-
-                }if(menusito==3){
-                    // Modificar un grafo 
-                   while(salir1==false){
-                        menusito2=v.menu2(); // Sub menu to modify a graph 
-                        // 
-                        if(menusito2==1){
-                            v.Departure();
-                            Csalida=scan.nextLine();
-                            v.Arrive();
-                            Cdestino=scan.nextLine();
-                            if (grafo.existe(Csalida, Cdestino)){
-                                grafo.agregar(Csalida,Cdestino,"100");
-                                v.exito();
-                                System.out.println("Ciudades Modificadas: " +"\n-"+ Csalida + "\n-"+ Cdestino);
-                            }else{
-                                v.Error();
-                            }
-                        }if(menusito2==2){
-                            v.Departure();
-                            Csalida=scan.nextLine();
-                            v.Arrive();
-                            Cdestino=scan.nextLine();
-                            v.distance();
-                            caminokm=scan.nextLine();
-                            if (grafo.existe(Csalida, Cdestino)){
-                                grafo.agregar(Csalida,Cdestino,caminokm);
-                                v.exito();
-                                System.out.println("Ciudades Modificadas: " +"\n-"+ Csalida + "\n-"+ Cdestino );
-                                System.out.println("La nueva distancia entre ciudades es de: "+caminokm);
-                            }else{
-                                v.Error();
-                            }
-
-                        }if (menusito2==3){
-                            // Return to main menu  
-                            v.back();
-                            salir1 =true;
-                        }
-                    }
-                }if (menusito==4){
-                    // Break the cycle and close the program 
-                    salir=true;
-                    v.despedida();
                 }
+            }if (menusito==2){
+                // Mostrar el centro del grafo 
+                v.Cg();
+                grafo.center();
+            }if(menusito==3){
+                // Modificar un grafo 
+                while(salir1==false){
+                    menusito2=v.menu2(); // Sub menu to modify a graph 
+                    // option of traffic disruption
+                    if(menusito2==1){
+                        v.Departure(); // Ask for the first city
+                        Csalida=scan.nextLine();
+
+                        v.Arrive();// Ask for the first city
+                        Cdestino=scan.nextLine();
+                        // If that checks if both cities exist 
+                        if (grafo.existe(Csalida, Cdestino)){
+                            grafo.agregar(Csalida,Cdestino,"100"); // add tho the graph the new data 
+                            //Shows the data changed 
+                            v.exito();
+                            System.out.println("Ciudades Modificadas: " +"\n-"+ Csalida + "\n-"+ Cdestino);
+                        }else{
+                            v.Error();
+                        }
+                    }if(menusito2==2){
+                        v.Departure();
+                        Csalida=scan.nextLine(); // Ask for the first city
+
+                        v.Arrive(); // Ask for the second city
+                        Cdestino=scan.nextLine();
+
+                        v.distance(); // Ask for the distance
+                        caminokm=scan.nextLine();
+                        // If that checks if both cities exist
+                        if (grafo.existe(Csalida, Cdestino)){
+                            grafo.agregar(Csalida,Cdestino,caminokm); // add tho the graph the new data
+                            //Shows the data changed 
+                            v.exito();
+                            System.out.println("Ciudades Modificadas: " +"\n-"+ Csalida + "\n-"+ Cdestino );
+                            System.out.println("La nueva distancia entre ciudades es de: "+caminokm);
+                        }else{
+                            v.Error();
+                        }
+
+                    }if (menusito2==3){
+                        // Return to main menu  
+                        v.back();
+                        salir1 =true;
+                    }
+                }
+            }if (menusito==4){
+                // Break the cycle and close the program 
+                salir=true;
+                v.despedida();
             }
         }
     }
+}
